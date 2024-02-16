248105fca270f30040446a833b697e2a0146e44d
Fisnik Hetemi <fisnikshetemi@gmail.com>
2024-02-16 17:33:22 +0100

    Pagination added

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
-// import Search from "../components/Search";
 import { Button, Col, Row } from "react-bootstrap";
-// import Sidebar from "../components/Sidebar";
 import BookCard from "../components/BookCard";
 import { useContext } from "react";
 import { HippoReadsContext } from "../assets/context/HippoReadsContext";
@@ -10,36 +8,78 @@ const Explore = () => {
   const [selectSortValue, setSelectSortValue] = useState("sort");
   const [library, setLibrary] = useState([]);
   const [search, setSearch] = useState("");
+  const [booksPerPage, setBooksPerPage] = useState(12);
+  const [currentPage, setCurrentPage] = useState(1);
+
   const { loading, setLoading, setActivePage, books } =
     useContext(HippoReadsContext);
   useEffect(() => {
     setActivePage("Explore");
   });
-
+  // |1 2 3| 4 5
   const getBooks = async () => {
-    let url_books = `https://65c5cbb5e5b94dfca2e04e3f.mockapi.io/hipporeads/books`;
+    let url_books = `https://65c5cbb5e5b94dfca2e04e3f.mockapi.io/hipporeads/books?p=${currentPage}&l=${booksPerPage}`;
 
     if (selectSortValue === "a_z") {
-      url_books += `?sortBy=name&order=asc`;
+      url_books += `&sortBy=name&order=asc`;
     } else if (selectSortValue === "z_a") {
-      url_books += `?sortBy=name&order=desc`;
+      url_books += `&sortBy=name&order=desc`;
     }
     if (search !== "") {
-      if(selectSortValue !== 'sort'){
-        url_books += `&name=${search}`;
-      }else{
-        url_books += `?name=${search}`;
-      }
+      url_books += `&name=${search}`;
     }
-
     await fetch(url_books)
       .then((res) => res.json())
-      .then((data) => setLibrary(data));
+      .then((data) => {
+        setLibrary(data);
+      });
   };
 
+  const nextPage = () => {
+    if (currentPage >= Math.ceil(books.length / booksPerPage)) {
+      setCurrentPage(1);
+      return;
+    }
+    setCurrentPage((prev) => prev + 1);
+  };
+  const getPaginationNumbers = () => {
+    let nums = [];
+    for (let i = 1; i <= Math.ceil(books.length / booksPerPage); i++) {
+      nums.push(i);
+    }
+
+    return nums;
+  };
+//1 2 3| 4 5|
+  const pointersOne = () => {
+    let pointer_one = 0;
+
+    if (currentPage > getPaginationNumbers()[pointer_one + 2]) {
+      pointer_one += 2;
+    }
+    return pointer_one;
+  };
+  const pointerstwo = () => {
+    let pointer_two = 3;
+
+    if (currentPage > getPaginationNumbers()[pointer_two -1]) {
+      pointer_two += 3;
+    }
+
+    return pointer_two;
+  };
+  const prevPage = () => {
+    if (currentPage == 1) {
+      setCurrentPage(Math.ceil(books.length / booksPerPage));
+      return;
+    }
+    setCurrentPage((prev) => prev - 1);
+  };
   useEffect(() => {
-    getBooks();
-  }, [selectSortValue, search]);
+    getBooks(); 
+    console.log(pointersOne())
+    console.log(pointerstwo())
+  }, [selectSortValue, search, currentPage]);
   return (
     <>
       {loading ? (
@@ -77,6 +117,26 @@ const Explore = () => {
                 </div>
               </div>
               <Row className="mt-5">
+                <div className="d-flex justify-content-end gap-2">
+                  <Button onClick={() => prevPage()}>
+                    <i className="fa-solid fa-arrow-left"></i>
+                  </Button>
+                  {getPaginationNumbers()
+                    .slice(pointersOne(), pointerstwo())
+                    .map((num) => (
+                      <button
+                        key={num}
+                        className={`btn ${
+                          currentPage == num ? " btn-danger" : "btn-success"
+                        }`}
+                      >
+                        {num}
+                      </button>
+                    ))}
+                  <Button onClick={() => nextPage()}>
+                    <i className="fa-solid fa-arrow-right"></i>
+                  </Button>
+                </div>
                 {library &&
                   library.map((book, i) => {
                     return (
